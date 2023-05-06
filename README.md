2be901d931aacf763c140ac8b8ac95e0f275ab0b
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2023-05-05 17:25:47 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Magic Auth Bitbucket Social Login
+
+Please [click here](https://magic.link/docs/auth/login-methods/social-logins/integration/social-providers/bitbucket) for full implementation instructions
---
+++ src/pages/Login.js
@@ -0,0 +1,27 @@
+import { magic } from "../lib/magic";
+import { FaBitbucket } from "react-icons/fa";
+
+const Login = () => {
+  const handleSocialLogin = async () => {
+    try {
+      await magic.oauth.loginWithRedirect({
+        provider: "bitbucket",
+        redirectURI: new URL("/dashboard", window.location.origin).href,
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1>Magic Auth + Bitbucket</h1>
+      <button onClick={handleSocialLogin}>
+        <FaBitbucket size={"2.5rem"} />
+        Log in with Bitbucket
+      </button>
+    </div>
+  );
+};
+
+export default Login;
